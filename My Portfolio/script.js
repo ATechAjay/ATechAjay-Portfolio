df47c0f5be96990c3ab463eb35b2ec6cf708c666
AJAY YADAV <imajydv8298@gmail.com>
2022-10-14 08:25:35 +0530

    remove onClick and add addEventListener to the back to top bottom

--- My Portfolio/script.js
+++ My Portfolio/script.js
@@ -1,18 +1,28 @@
-let mybutton = document.getElementById("backToTopButton");
+let myButton = document.getElementById("backToTopButton");
 
-window.onscroll = function() {showScrollButton()};
+window.onscroll = function () {
+  showScrollButton();
+};
 
 // When the user scrolls down 500px from the top of the document, show the button
 function showScrollButton() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    mybutton.style.display = "block";
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
+    myButton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    myButton.style.display = "none";
   }
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function scrollToTop() {
+// function scrollToTop() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
+
+myButton.addEventListener("click", function scrollToTop() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+});
